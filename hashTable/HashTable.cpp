9b3fe40421369811ae1ca7d722002bce32be9532
damiangohrh123 <damiangohrh@gmail.com>
2024-05-29 17:54:55 +0800

    Add main.cpp. Remove initilization for a,c,m variable.

--- hashTable/HashTable.cpp
+++ hashTable/HashTable.cpp
@@ -3,8 +3,10 @@
 #include <iostream>
 
 HashTable::HashTable(long _a, long _c, long _m)
-  : a(_a), c(_c), m(_m)
 {
+  a = _a;
+  c = _c;
+  m = _m;
   buckets.resize(m, -1);
 }
 
@@ -133,31 +135,4 @@ void HashTable::printTable()
 int HashTable::hashNumberCalculator(int key)
 {
   return (a * key + c) % m;
-}
-
-int main()
-{
-  long a = 2;
-  long c = 0;
-  long m = 5;
-
-  HashTable ht(a, c, m);
-  ht.insert(7);
-  ht.insert(6);
-  ht.insert(3);
-  ht.insert(2);
-  ht.insert(4);
-  ht.insert(11);
-
-  ht.printTable();
-
-  std::cout << "Removing 3" << "\n";
-  ht.remove(3);
-
-  ht.printTable();
-
-  std::cout << ht.find(8) << "\n";
-  std::cout << ht.find(11) << "\n";
-
-  return 0;
 }
